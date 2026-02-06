f30d8d6b7aa1ba5ea98484052ee0d0d2469b5156
omedcodes <omedsafi268@gmail.com>
2026-02-06 22:12:28 +0100

    feat: structuring variables and mathematical logic for spinning donut

--- donut.c
+++ donut.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
+#include <string.h>
+int k;
+
+double sin(),cos();
+
 int main() 
 {
-    printf("hello world");
+    float A = 0, B = 0, i, j, z[1760];
+    char b[1760];
+    printf("\n1b2J");
+    for (;;){
+        memset(b, 32, 1760);
+        memset(z, 0, 7040);
+        for(j = 0; 6.38 > j; j+=0.01) {
+            for (i = 0; 6.28 > i; i+= 0.08) {
+                float c = sin(i), d = cos(j), e = sin(A), f = sin(j), g = cos(A),
+                      h = d + 2, D = 1 / (c * h * e + f * g + 5), l = cos(i),
+                      m = cos(B), n = sin(B), t = c * h * g - f * c;
+            }
+        }
+    }
     return 0;
 }
