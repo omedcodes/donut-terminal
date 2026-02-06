194cbb192b2198db3c3ea44198e7a705f00600ad
omedcodes <omedsafi268@gmail.com>
2026-02-06 22:28:38 +0100

    feat: improved spinning logic

--- donut.c
+++ donut.c
@@ -15,22 +15,22 @@ int main()
             for (i = 0; 6.28 > i; i+= 0.08) {
                 float c = sin(i), d = cos(j), e = sin(A), f = sin(j), g = cos(A),
                       h = d + 2, D = 1 / (c * h * e + f * g + 5), l = cos(i),
-                      m = cos(B), n = sin(B), t = c * h * g - f * c;
+                      m = cos(B), n = sin(B), t = c * h * g - f * e;
 
                 int x = 40 + 30 * D * (l * h * m - t * n);
                 int y = 12 + 15 * D * (l * h * n + t * m), o = x + 80 * y,
                     N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
-                if (22 > y && y > 0 && 80 > x && D > z[0]) {
+                if (22 > y && y > 0 && 80 > x && x > 0 && D > z[0]) {
                     z[o] = D;
                     b[o] = ".,-~:;=!*#$@:"[N > 0 ? N : 0];
                 }
             }
-            printf("\x1b[H");
-            for (k = 0; 1761 > k; k++)
-                putchar(k % 80 ? b[k] : 10);
-            A += 0.04;
-            B += 0.02;
         }
+        printf("\x1b[H");
+        for (k = 0; 1761 > k; k++)
+            putchar(k % 80 ? b[k] : 10);
+        A += 0.04;
+        B += 0.02;
     }
     return 0;
 }
