9d067fc5b4e944cd40f5f3ae0f3c7cf81727d5b7
omedcodes <omedsafi268@gmail.com>
2026-02-06 22:46:03 +0100

    feat: complete spinning donut and add last adjustments

--- donut.c
+++ donut.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-int k;
 
 int main() 
 {
+    int k;
     float A = 0, B = 0, i, j, z[1760];
     char b[1760];
-    printf("\n1b[2J");
+    printf("\n\n\x1b[2J");
     for (;;){
         memset(b, 32, 1760);
         memset(z, 0, 7040);
-        for(j = 0; 6.38 > j; j+=0.01) {
-            for (i = 0; 6.28 > i; i+= 0.08) {
+        for(j = 0; 6.38 > j; j+=0.05) {
+            for (i = 0; 6.28 > i; i+= 0.09) {
                 float c = sin(i), d = cos(j), e = sin(A), f = sin(j), g = cos(A),
                       h = d + 2, D = 1 / (c * h * e + f * g + 5), l = cos(i),
                       m = cos(B), n = sin(B), t = c * h * g - f * e;
